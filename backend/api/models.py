e5f8639ace93ccf746c75edb14b390e7f468dc99
Analice <lea5jvl@bosch.com>
2024-06-05 11:30:03 -0300

    including api's urls

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # If I delete an User, all of his notes are goin to be deleted too, this is what CASCADE do
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    
+    def __str__(self):
+        return self.title
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('api/user/register/', CreateUserView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
-    path('api/auth/', include('rest_framework.urls'),),
+    path('api/auth/', include('rest_framework.urls')),
+    # Se for para algum lugar que contenha api/ e nao for nenhuma das opções anteriores, vai para a url da api e procurar lá
+    path("api/", include("api.urls")),
 ]
